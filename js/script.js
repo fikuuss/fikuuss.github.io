c3d04f37fe285bdac33d3558cb4401d5a6dd28f5
fikuuss <manwithcane333@gmail.com>
2016-01-15 18:05:04 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var articles = { "article1" [
+    "title": "unknown title",
+    "tags": [
+        "tag1", "tag2", "tag3"
+    ],
+    "date": "unknown date",
+    "author": "unknown author",
+    "description": "article description",
+    "fullArticle": "full text of article"
+]};
+
+console.log(JSON.stringify(article, null, "\t"));
