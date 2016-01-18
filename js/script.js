6d7abb278a908f83b185c930f992617c0075f837
fikuuss <manwithcane333@gmail.com>
2016-01-18 15:03:34 +0200

    ver. "with articles"

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
-var articles = { "article1" [
-    "title": "unknown title",
-    "tags": [
-        "tag1", "tag2", "tag3"
-    ],
-    "date": "unknown date",
-    "author": "unknown author",
-    "description": "article description",
-    "fullArticle": "full text of article"
-]};
+_.templateSettings.variable = "articles";
 
-console.log(JSON.stringify(article, null, "\t"));
+var template = _.template(
+    $("script.template").html()
+);
+
+var articles =  [
+    {
+        id: 1,
+        title: "unknown title",
+        tags: [
+            "tag1", "tag2", "tag3"
+        ],
+        date: "unknown",
+        author: "unknown author",
+        fullArticle: "full text of article"
+    }, {
+        id: 2,
+        title: "unknown title",
+        tags: [
+            "tag1", "tag2", "tag3"
+        ],
+        date: "unknown",
+        author: "unknown author",
+        fullArticle: "full text of article"
+    },  {
+        id: 3,
+        title: "unknown title",
+        tags: [
+            "tag1", "tag2", "tag3"
+        ],
+        date: "unknown",
+        author: "unknown author",
+        fullArticle: "full text of article"
+    }
+];
+
+
+$(".content-blocks").append(
+    template(articles)
+);
