fefa9f7e6ca1892449e8fc111a47aea4ee6d7196
fikuuss <manwithcane333@gmail.com>
2016-01-20 16:55:04 +0200

    ver. "working, not clear"

--- js/script.js
+++ js/script.js
@@ -1,13 +1,56 @@
 $(document).ready(function() {
-    _.templateSettings.variable = "articles";
+    $.get("js/articles.json", {}, function(data) {
+        function createAllTags() {
+            var allTags = [];
+            _.each(data.articles, function(article) {
+                _.each(article.tags, function(tag) {
+                    allTags.push(tag);
+                });
+            });
 
-    var template = _.template(
-        $("script.template").html()
-    );
+            return allTags;
+        }
 
-    $.get("js/articles.json", {}, function(json) {
-        $(".content-blocks").append(
-            template(json.articles)
+        var allTags = _.uniq(createAllTags());
+
+        //BEGIN: display articles template
+        _.templateSettings.variable = "articles";
+
+        var articlesTemplate = _.template(
+            $("script.articles-template").html()
         );
+
+        $(".content-blocks").append(
+            articlesTemplate(data.articles)
+        );
+        //END: display articles template
+
+        //BEGIN: display tags template
+        _.templateSettings.variable = "tags";
+
+        var tagsTemplate = _.template(
+            $("script.tags-template").html()
+        );
+
+        $(".search-tags").append(
+            tagsTemplate(allTags)
+        );
+        //END: display tags template
+
+        //BEGIN: tags control
+        var selectedTags = [];
+
+        $('form').on('change', 'input[type="checkbox"]', function() {
+            if ($("#" + this.id).prop("checked"))
+            {
+                selectedTags.push(this.name);
+            }
+            else {
+                selectedTags = _.without(selectedTags, this.name);
+            }
+
+        });
+
+        //END: tags control
     });
 });
